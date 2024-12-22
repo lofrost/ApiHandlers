f2eb5ad00d61d21143707152357dd9fbe04bbb31
lofrost <lofrost20005@gmail.com>
2024-12-22 18:46:01 +0300

    Im done today

--- Test.go
+++ Test.go
@@ -1,22 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var task int
+type requestBody struct {
+	Message string `json:"message"`
+}
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+var task string
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	d := json.NewDecoder(r.Body)
+
+	var req requestBody
+
+	d.Decode(&req)
+
+	task = req.Message
+	fmt.Fprintln(w, "Task succesfully updated")
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "hello, %s", task)
 }
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/update", PostHandler).Methods("POST")
+	router.HandleFunc("/api/task", GetHandler).Methods("GET")
 	http.ListenAndServe(":8080", router)
 
 }
