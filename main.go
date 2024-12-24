addad31d6c0d487a9ab91b13f8c568c02f7fbacd
lofrost <lofrost20005@gmail.com>
2024-12-24 19:30:30 +0300

    I finished lesson part 2

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 type requestBody struct {
-	Message string `json:"message"`
+	Msg string `json:"message"`
 }
 
 var task string
@@ -17,20 +18,29 @@ var task string
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	d := json.NewDecoder(r.Body)
 
-	var req requestBody
+	var req Message
 
 	d.Decode(&req)
-
-	task = req.Message
-	fmt.Fprintln(w, "Task succesfully updated")
+	err := DB.Create(&req).Error
+	if err != nil {
+		log.Fatal("Failed to create Task in DB")
+	}
+	fmt.Fprintln(w, "Task succesfully created")
 }
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprintf(w, "hello, %s", task)
+	var tasks []Message
+	DB.Find(&tasks)
+	//fmt.Fprintln(w, tasks)
+	for _, task := range tasks {
+		fmt.Fprintf(w, "Task - %v\nIsDone - %v\nCreated at - %v\n\n", task.Task, task.IsDone, task.CreatedAt)
+	}
 }
 
 func main() {
+	InitDB()
+
+	DB.AutoMigrate(&Message{})
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/update", PostHandler).Methods("POST")
